7597205b2cc26134ab4157ea03b90e31458c495f
kyama-git <kota.yamasaki@broadcom.com>
2025-02-03 15:15:38 +0900

    new files

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1:0ebf0cc7-932a-4f5b-933a-42dc45d35b52', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'd3c6020fnfk2wt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
